bf6f30e190a4def3fb6e65db728dc6c625afcc1b
Danil Sergeev <danil.kzn2003@gmail.com>
2024-11-07 15:39:00 +0100

    feature: initial division

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
@@ -8,28 +6,52 @@ function App() {
 
   return (
     <>
+      <header>
+        <div>Nav bar here</div>
+      </header>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <p>Hello</p>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
+
+      <div id="quote and picture">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+      <div id="carousel"></div>
+
+      <div id="members"></div>
+
+      <div id="cities"> </div>
+
+      <div id="socials"></div>
     </>
   )
+  // return (
+  //   <>
+  //     <div>
+  //       <a href="https://vite.dev" target="_blank">
+  //         <img src={viteLogo} className="logo" alt="Vite logo" />
+  //       </a>
+  //       <a href="https://react.dev" target="_blank">
+  //         <img src={reactLogo} className="logo react" alt="React logo" />
+  //       </a>
+  //     </div>
+  //     <h1>Vite + React</h1>
+  //     <div className="card">
+  //       <button onClick={() => setCount((count) => count + 1)}>
+  //         count is {count}
+  //       </button>
+  //       <p>
+  //         Edit <code>src/App.tsx</code> and save to test HMR
+  //       </p>
+  //     </div>
+  //     <p className="read-the-docs">
+  //       Click on the Vite and React logos to learn more
+  //     </p>
+  //   </>
+  // )
 }
 
 export default App
---
+++ src/components/navbar.tsx
@@ -0,0 +1,12 @@
+function Navbar() {
+  return (
+    <>
+      <header>{/* <Header withUserInfo={false} /> */}</header>
+      <div>
+        <p>Hello</p>
+      </div>
+    </>
+  )
+}
+
+export default Navbar
