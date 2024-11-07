45ca11dba9c1b21bc911441fcc07fc52c4fbebb3
Danil Sergeev <danil.kzn2003@gmail.com>
2024-11-07 20:22:59 +0100

    feature: add skeleton and example components

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,99 @@
-import { useState } from 'react'
+// import { useState } from 'react'
 import './App.css'
+import IMAGES from './images/Images'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
-    <>
+    <div>
+      {/* <button
+            onClick={() => setCount((count) => count + 1)}
+            className="justify-self-end"
+          >
+            count is {count}
+          </button> */}
       <header>
-        <div>Nav bar here</div>
+        <div className="bg-pink-300">Nav bar here</div>
       </header>
-      <div>
-        <p>Hello</p>
-      </div>
+      <main className="flex flex-col gap-5 bg-orange-400">
+        <div
+          id="quote and picture"
+          className="bg-green-300 border-2 flex flex-col items-center"
+        >
+          <img
+            src={IMAGES.poster}
+            alt="first image"
+            className="object-cover h-96 w-[90%] p-10 "
+          />
 
-      <div id="quote and picture">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
+          <div className="bg-purple-400 w-1/3">
+            Band genre desriptiong + quote
+          </div>
+        </div>
 
-      <div id="carousel"></div>
+        {/* TODO: shadcn Carousel compnent❎ */}
 
-      <div id="members"></div>
+        <div id="carousel" className="flex flex-row bg-blue-400 border-2 ">
+          <img
+            src={IMAGES.song1}
+            alt="first image"
+            className="rounded-3xl object-cover h-1/3 w-1/3 min-h-36 min-w-80"
+          />
+          {/* <img src={IMAGES.song2} alt="first image" />
+          <img src={IMAGES.song3} alt="first image" />
+          <img src={IMAGES.song4} alt="first image" /> */}
+        </div>
 
-      <div id="cities"> </div>
+        <div
+          id="members"
+          className="bg-purple-400 flex flex-col gap-10 border-2"
+        >
+          <div className="flex flex-row gap-10">
+            <h2>Name 1</h2>
+            <img src={IMAGES.tyler} alt="first image" className="rounded-3xl" />
+          </div>
 
-      <div id="socials"></div>
-    </>
+          <div className="flex flex-row gap-10">
+            <img src={IMAGES.josh} alt="first image" />
+            <h2>Name 2</h2>
+          </div>
+        </div>
+
+        {/* TODO: shadcn Accordion component❎ */}
+
+        <div
+          id="questions"
+          className="bg-lime-400 flex flex-col gap-4 border-2"
+        >
+          <h2>► Accordion 1</h2>
+          <h2>► Accordion 2</h2>
+          <h2>► Accordion 3</h2>
+        </div>
+
+        {/* TODO: shadcn Avatar component❎❓❓ */}
+
+        <div id="socials" className="bg-amber-400 border-2">
+          <p>--Separator--</p>
+          <div>
+            <p>Instagram</p>
+            <img src={IMAGES.inst} alt="instagram image" />
+          </div>
+          <div>
+            <p>Youtube</p>
+            <img src={IMAGES.spot} alt="youtube image" />
+          </div>
+          <div>
+            <p>Spotify</p>
+            <img src={IMAGES.yout} alt="spotify image" />
+          </div>
+          <div>
+            <p>Facebook</p>
+            <img src={IMAGES.fcb} alt="facebook image" />
+          </div>
+        </div>
+      </main>
+    </div>
   )
   // return (
   //   <>
---
+++ src/images/Images.tsx
@@ -0,0 +1,16 @@
+const IMAGES = {
+  logo: new URL('./logo.png', import.meta.url).href,
+  poster: new URL('./wide.jpg', import.meta.url).href,
+  song1: new URL('./song1.png', import.meta.url).href,
+  song2: new URL('./song2.jpg', import.meta.url).href,
+  song3: new URL('./song3.jpg', import.meta.url).href,
+  song4: new URL('./song4.jpg', import.meta.url).href,
+  tyler: new URL('./Tyler.png', import.meta.url).href,
+  josh: new URL('./Josh.png', import.meta.url).href,
+  inst: new URL('./instagram.png', import.meta.url).href,
+  yout: new URL('./youtube.png', import.meta.url).href,
+  fcb: new URL('./facebook.png', import.meta.url).href,
+  spot: new URL('./spotify.png', import.meta.url).href,
+}
+
+export default IMAGES
