273ad1b2493584f12f17087f9c7237684ad55785
Danil Sergeev <danil.kzn2003@gmail.com>
2024-11-11 16:57:46 +0100

    fix: components extraction into files from navigations

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import './App.css'
-import IMAGES from './images/Images'
 
 import { Separator } from './components/ui/separator'
 
@@ -8,49 +7,13 @@ import SongsCarousel from './components/SongsCarousel'
 import BandMembers from './components/BandMembers'
 import Questions from './components/Questions'
 import Socials from './components/Socials'
+import Navigations from './components/Navigations'
 
 function App() {
-  const paragraphs = ['main', 'songs', 'members', 'questions', 'socials']
-  const liParagraphs = paragraphs.map((elem) => (
-    <li>
-      {
-        <a href={`#${elem}`} className="hover:underline underline-offset-8">
-          {elem}
-        </a>
-      }
-    </li>
-  ))
-
   return (
     <div>
       <header className="px-14 max-w-screen fixed z-10">
-        <div
-          id="navbar"
-          className="font-typeface w-[92.7%] bg-red-700 flex justify-between fixed  text-7xl font-extralight tracking-wide "
-        >
-          <div
-            id="logo and name"
-            className="text-6xl flex gap-4 p-3 px-7 items-center"
-          >
-            <img
-              src={IMAGES.logo}
-              alt="logo"
-              className="object-cover h-9 w-9"
-            />
-            <div>Twenty One Pilots</div>
-          </div>
-          <div
-            id="navigations and website"
-            className="flex gap-14 p-3 px-7 text-4xl items-center"
-          >
-            <div id="navigations" className="gap-24">
-              <ul className=" flex flex-row gap-4">{liParagraphs}</ul>
-            </div>
-            <a href="https://www.twentyonepilots.com/">
-              <div id="website">website</div>
-            </a>
-          </div>
-        </div>
+        <Navigations />
       </header>
 
       <main className="px-14 max-w-screen bg-black flex flex-col gap-24 ">
