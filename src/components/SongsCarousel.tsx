574a7d438481d8d33575ff031f43e0fcb9332672
Danil Sergeev <danil.kzn2003@gmail.com>
2024-11-11 17:24:52 +0100

    fix: issue with video carousel expanshion on hover

--- src/components/SongsCarousel.tsx
+++ src/components/SongsCarousel.tsx
@@ -11,29 +11,29 @@ function SongsCarousel() {
   const listItems = songs.map((song) => (
     <CarouselItem
       key={song.id}
-      className=" aspect-video basis-1/3 items-center justify-center p-8 "
+      className=" aspect-video basis-1/3 justify-center p-4"
     >
       <a href={song.href}>
         <img
           src={song.image}
           alt={`${song.name} song preview image`}
-          className="transition ease-in-out delay-150 hover:scale-110 rounded-3xl object-cover max-h-full h-64 w-96 min-h-36 min-w-80 justify-self-center"
+          className="transition ease-in-out delay-150 hover:scale-110 rounded-3xl object-cover max-h-[70%]  h-64 w-96 min-h-1/2 min-w-1/2 justify-self-center"
         />
       </a>
-      <h3 className="font-mono py-5 text-center text-zinc-400">{song.name}</h3>
+      <h3 className="font-mono pt-5 text-center text-zinc-400">{song.name}</h3>
     </CarouselItem>
   ))
 
   return (
-    <div className="flex flex-col align-middle justify-evenly gap-5">
+    <div className="flex flex-col align-middle justify-evenly gap-10 ">
       <h2 className="text-3xl text-white font-bold px-24">
         My favourite songs
       </h2>
-      <Carousel className=" px-8 bg-black w-[90%] mx-10">
-        <CarouselContent className="h-min align-middle justify-evenly flex">
+      <Carousel className=" px-8 bg-black w-[90%] self-center">
+        <CarouselContent className=" align-middle justify-evenly flex mx-4">
           {listItems}
         </CarouselContent>
-        <CarouselPrevious className="bg-gray-400 border-gray-400" />
+        <CarouselPrevious className="bg-gray-400 border-gray-400 " />
         <CarouselNext className="bg-gray-400 border-gray-400" />
       </Carousel>
     </div>
